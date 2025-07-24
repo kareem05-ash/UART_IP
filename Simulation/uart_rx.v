e818de1504e3238050871b2f1c938aa525b42045
Kareem Ashraf <ashrafkareem825@gmail.com>
2025-07-24 05:25:34 +0300

    add uart_rx.v 'RX' module with parameters {BAUD, clk_freq, clk_period, oversampling_rate, data_wd, parity}

--- Simulation/uart_rx.v
+++ Simulation/uart_rx.v
@@ -1,148 +1,172 @@
+`timescale 1ns/1ps
 module uart_rx#
 (
-    parameter BAUD = 9600,                      //baud rate per second
-    parameter clk_freq = 50_000_000,            //system clk frequency
-    parameter oversampling_rate = 16,           //to maintain valid data (avoiding noise) : transimits data @ tick-7 pulse
-    parameter data_wd = 8,                      //data width
-    parameter [1:0] parity = 0                  //1:odd-parity, 2:even-parity, default:no-parity
+    parameter BAUD = 9600,                          //baud rate per second
+    parameter clk_freq = 50_000_000,                //system clk frequency in 'HZ'
+    parameter clk_period = 1_000_000_000/clk_freq,  //system clk period in 'ns'
+    parameter oversampling_rate = 16,               //to maintain valid data (avoiding noise)
+    parameter data_wd = 8,                          //data width 
+    parameter parity = 0                            //1:odd, 2:even, default:no-parity
 )
 (
-    input clk,                                  //system clk
-    input rst,                                  //system async. active-high reset
-    input rx,                                   //serial data-in
-    input tick,                                 //pulse from BAUD Generator
-    input rx_start,                             //allows recieving operation
-    output reg rx_done,                         //indicates a single frame's been recieved successfully
-    output reg rx_busy,                         //indicates that a frame is being recieved
-    output reg framing_error_flag,              //indicates errors (noise, incorrect start or stop bits)
-    output reg parity_error_flag,               //indicates parity errors. if parity-bit recieved doesn't equall parity_expected
-    output reg [data_wd-1 : 0] dout             //parallel output data
+    input wire clk,                                 //system clk
+    input wire rst,                                 //system async. active-high reset
+    input wire rx,                                  //serial input data
+    input wire tick,                                //pulse from baud generator
+    input wire rx_start,                            //signal allows receiving operation
+    output reg rx_done,                             //indicates that a single frame's been received successfully 
+    output reg rx_busy,                             //indicates that a single frame is being received
+    output reg parity_error_flag,                   //indicates parity errors. if parity received doesn't equal parity expected
+    output reg framing_error_flag,                  //indicates start-bit, stop-bit, or data-bits error (noise)
+    output reg [data_wd-1 : 0] dout                 //parallel output data
 );
-    localparam IDLE   = 3'b000,                 //waits for tx_start = 1
-               START  = 3'b001,                 //start-bit = 0
-               DATA   = 3'b011,                 //transmit 8-bit data (LSB -> MSB)
-               PARITY = 3'b010,                 //sending parity bit if enabled
-               STOP   = 3'b110,                 //stop-bit = 1
-               DONE   = 3'b111;                 //raise tx_done. go back to IDLE
 
-    reg bit_test;   //to hold the value of rx @(tick_count = 0) to compare it with rx @(tick_count == 7)
-    reg[2:0] c_state, n_state;
-    reg[$clog2(oversampling_rate)-1 : 0] tick_count;
-    reg[$clog2(data_wd)-1 : 0] bit_index;
-    reg[data_wd-1 : 0] recieved_data;
-    wire parity_en = (parity == 1 || parity == 2)? 1 : 0;
-    wire parity_expected = (parity == 1)? ^recieved_data : ((parity == 2)? ~^recieved_data : 0);
+    //states encoding (One-Hot) to minimize glitches
+    localparam IDLE   = 6'b000001,                  //waits for tx_start = 1
+               START  = 6'b000010,                  //start-bit = 0
+               DATA   = 6'b000100,                  //transmit 8-bit data (LSB -> MSB)
+               PARITY = 6'b001000,                  //sending parity bit if enabled
+               STOP   = 6'b010000,                  //stop-bit = 1
+               DONE   = 6'b100000;                  //raise tx_done. go back to IDLE
 
-    //state transitions (sequential)
+    reg test_bit;                                   //holds received bit @(tick = 0) ot campare it with received bit @(tick = oversampling_rate/2)
+    reg [5:0] c_state, n_state;                     //state registers
+    reg [data_wd-1 : 0] received_data;              //register stores the data bits until the frame stops correctly to assign it to 'dout'
+    reg [$clog2(data_wd+1)-1 : 0] bit_index;        //counts data bits received
+    reg [$clog2(oversampling_rate)-1 : 0] tick_count;   
+    wire parity_en = (parity == 1 || parity == 2);   
+    wire parity_expected = (parity == 1)? ^received_data : ~^received_data;
+
+    //state transition logic (sequential)
     always@(posedge clk or posedge rst)
-        if(rst)
-            c_state <= IDLE;
-        else
+        if(rst || framing_error_flag || parity_error_flag)
+            c_state <= IDLE;                        //default state
+        else       
             c_state <= n_state;
 
-    //n_state handeling (combinational)
+    //n_state logic (combinational)
     always@(*)
-        begin
-            case(c_state)
-                IDLE    : n_state = rx_start? START : IDLE;
-                START   : n_state = (tick_count == (oversampling_rate-1))? DATA : START;
-                DATA    : n_state = ((tick_count == (oversampling_rate-1)) && (bit_index == data_wd-1))? (parity_en? PARITY : STOP) : DATA;
-                PARITY  : n_state = (tick_count == (oversampling_rate-1))? STOP : PARITY;
-                STOP    : n_state = (tick_count == (oversampling_rate-1))? DONE : STOP;
-                DONE    : n_state = rx_done? IDLE : DONE;
-                default : n_state = IDLE;
-            endcase
-        end 
+        case(c_state)
+            IDLE    : n_state = rx_start? START : IDLE;       
+            START   : n_state = (tick_count == (oversampling_rate-1))? DATA : START;
+            DATA    : n_state = ((tick_count == (oversampling_rate-1)) && (bit_index == data_wd))? (parity_en? PARITY : STOP) : DATA;
+            PARITY  : n_state = (tick_count == (oversampling_rate-1))? STOP : PARITY;
+            STOP    : n_state = (tick_count == (oversampling_rate-1))? DONE : STOP;
+            DONE    : n_state = rx_done? IDLE : DONE;   //rx_done is raised means that the frame's received successfully.
+            default : n_state = IDLE;
+        endcase
 
-    //output assigning and counters
+    //outputs and counters logic (sequential)
     always@(posedge clk or posedge rst)
         begin
             if(rst)
                 begin
-                    dout <= 0;
+                    framing_error_flag <= 0;
+                    parity_error_flag <= 0;
+                    received_data <= 0;
+                    tick_count <= 0;
+                    bit_index <= 0;
+                    test_bit <= 0;
                     rx_done <= 0;
                     rx_busy <= 0;
-                    bit_index <= 0;
-                    tick_count <= 0;
-                    // recieved_data <= 0;
-                    parity_error_flag <= 0;
-                    framing_error_flag <= 0;
                 end
             else
-                case(c_state)   
-                    IDLE    : 
-                            begin
-                                rx_busy <= rx_start;
-                                rx_done <= 0;
-                                bit_index <= 0;
-                                tick_count <= 0;
-                                // recieved_data <= 0;
-                                parity_error_flag <= 0;
-                                framing_error_flag <= 0;
-                            end
-                    START   : 
-                            if(tick_count == 7)
-                                if(rx)
-                                    framing_error_flag <= 1;        //start-bit error
-                                else    
+                begin
+                    case(c_state)
+                        IDLE    : 
+                                begin
                                     framing_error_flag <= 0;
-                    DATA    : 
-                            begin
-                                if(tick_count == 0)
-                                    bit_test <= rx;
-                                if(tick_count == 7)
-                                    if(bit_test == rx)
-                                        begin
-                                            recieved_data[bit_index] <= rx;
-                                            bit_index <= bit_index + 1;
-                                            framing_error_flag <= 0;
-                                        end 
-                                    else    
-                                        framing_error_flag <= 1;    //noise at recieved bit
-                            end
-                    PARITY  : 
-                            if(tick_count == 7)
-                                if(rx == parity_expected)
                                     parity_error_flag <= 0;
-                                else
-                                    parity_error_flag <= 1;
-                    STOP    : 
-                            if(tick_count == 7)
-                                if(rx)      //valid stop-bit
+                                    received_data <= 0;
+                                    bit_index <= 0;     //reset the counter after the previous frame (bit_count was 8)
+                                    rx_done <= 0;
+                                    rx_busy <= 0;       //waits for 'rx_start' signal to be raised
+                                    if(rx_start)
+                                        begin
+                                            tick_count <= 0;    //reset the counter to sample start-bit
+                                            rx_busy <= 1;       //start receiving the frame
+                                        end
+                                end
+                        START   : 
+                                begin
+                                    bit_index <= 0;             //reset the counter to maintain the expected behavior (receiving all data bits)
+                                    if(tick)
+                                        begin
+                                            if(tick_count == 1)
+                                                test_bit <= rx;             //storing the start-bit
+                                            if((tick_count == oversampling_rate/2) && ((test_bit != rx) || rx))
+                                                framing_error_flag <= 1;    //noise on start-bit
+                                        end
+                                end
+                        DATA    : 
+                                begin
+                                    if(tick)
+                                        begin
+                                            if(tick_count == 1)
+                                                test_bit <= rx;     //storing data bit to sample it 
+                                            if(tick_count == oversampling_rate/2)
+                                                if(test_bit != rx)
+                                                    framing_error_flag <= 1;    //noise on data bits
+                                                else    
+                                                    begin
+                                                        received_data [bit_index] <= rx;
+                                                        bit_index <= bit_index + 1;     //increment the counter to receive the next bit
+                                                    end
+                                        end
+                                end
+                        PARITY  : 
+                                begin
+                                    if(tick)
+                                        begin
+                                            if(tick_count == 1)
+                                                test_bit <= rx;     //storing the parity bit to sample it 
+                                            if(tick_count == oversampling_rate/2)
+                                                if(test_bit != rx)
+                                                    framing_error_flag <= 1;    //noise on parity bit
+                                                else if(rx != parity_expected)
+                                                    parity_error_flag <= 1;     //parity error 
+                                        end
+                                end
+                        STOP    : 
+                                begin
+                                    if(tick)
+                                        begin
+                                            if(tick_count == 1)
+                                                test_bit <= rx;     //storing the stop-bit
+                                            if((tick_count == oversampling_rate/2) && ((test_bit != rx) || !rx))
+                                                framing_error_flag <= 1;    //noise on stop-bit                                            
+                                        end
+                                end
+                        DONE    : 
+                                begin
+                                    dout <= received_data;  //assigning the received data to the output data port
+                                    rx_done <= 1;           //indicates valid reciption process
+                                end
+                        default :
+                                begin       //reset operation
                                     framing_error_flag <= 0;
-                                else 
-                                    framing_error_flag <= 1;
-                    DONE    : 
-                            begin
-                                dout <= recieved_data;
-                                rx_busy <= 0;
-                                rx_done <= 1;
-                            end
-                    default : 
-                            begin
-                                dout <= 0;
-                                rx_done <= 0;
-                                rx_busy <= 0;
-                                bit_index <= 0;
-                                tick_count <= 0;
-                                recieved_data <= 0;
-                                parity_error_flag <= 0;
-                                framing_error_flag <= 0;
-                            end
-                endcase
-
-            if(tick)        //tick_count logic
+                                    parity_error_flag <= 0;
+                                    received_data <= 0;
+                                    tick_count <= 0;
+                                    bit_index <= 0;
+                                    test_bit <= 0;
+                                    rx_done <= 0;
+                                    rx_busy <= 0;
+                                    dout <= 0;
+                                end
+                    endcase
+                end
+            // tick counter logic
+            if(tick)    
                 if(tick_count == oversampling_rate-1)
-                    tick_count <= 0;
-                else
-                    tick_count <= tick_count + 1;
-            
+                    tick_count <= 0;                //reset the counter
+                else        
+                    tick_count <= tick_count + 1;   //increment the counter if not full
+            // framing, parity errors handling
             if(framing_error_flag || parity_error_flag)
                 begin
-                    n_state = IDLE;
-                    dout = 0;
-                    $display("Error : framing_error_flag = %d, parity_error_flag = %d, dout = %h, c_state = %b, bit_index = %d",
-                             framing_error_flag, parity_error_flag, dout, c_state, bit_index);
+                    n_state <= IDLE;
+                    dout <= 0;
                 end
-        end
+        end 
 endmodule
