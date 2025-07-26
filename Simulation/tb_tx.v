e5aca9764a8db08b1332d0aaa52758d3c4ec85e5
Kareem Ashraf <ashrafkareem825@gmail.com>
2025-07-26 07:11:55 +0300

    edit tb_tx.v to make it a strong self-checking tb. add 1st scenario (Reset Behavior)

--- Simulation/tb_tx.v
+++ Simulation/tb_tx.v
@@ -6,31 +6,34 @@
 ////////////////////////////////////////////////////
 `timescale 1ns/1ps
 module tb_tx();
-    parameter BAUD = 9600;              //baud rate per second
-    parameter clk_freq = 50_000_000;    //system clk frequency
-    parameter oversampling_rate = 16;   //to maintain valid data (avoiding noise)
-    parameter data_wd = 8;              //data width
-    parameter [1:0] parity = 0;         //1:odd-parity, 2:even-parity, default:no-parity
+    parameter BAUD = 9600;                          //baud rate per second
+    parameter clk_freq = 50_000_000;                //system clk frequency
+    parameter clk_period = 1_000_000_000/clk_freq;  //system clk period in 'ns'
+    parameter oversampling_rate = 16;               //to maintain valid data (avoiding noise)
+    parameter data_wd = 8;                          //data width
+    parameter [1:0] parity = 1; //odd-parity        //1:odd-parity, 2:even-parity, default:no-parity
 
-    reg clk;                            //system clk
-    reg rst;                            //system async. active-high reset
-    reg tx_start;                       //signal to initiate data (allow the transimition operation)
-    reg [data_wd-1 : 0] din;            //parallel input data to be tarnsimitted
-    wire tick;                          //pulse to transimit one bit (from baud generator)
-    wire tx;                            //serial parallel output line
-    wire tx_done;                       //flag indicates that the trans. operation is done
-    wire tx_busy;                       //flag indicates that the trans. operation is excuting
+    reg clk;                                        //system clk
+    reg rst;                                        //system async. active-high reset
+    reg tx_start;                                   //signal to initiate data (allow the transimition operation)
+    reg [data_wd-1 : 0] din;                        //parallel input data to be tarnsimitted
+    wire tick;                                      //pulse to transimit one bit (from baud generator)
+    wire tx;                                        //serial parallel output line
+    wire tx_done;                                   //flag indicates that the trans. operation is done
+    wire tx_busy;                                   //flag indicates that the trans. operation is excuting
 
-    localparam IDLE   = 3'b000,         //waits for tx_start = 1
-               START  = 3'b001,         //start-bit = 0
-               DATA   = 3'b011,         //transimate 8-bit data : LSB (firts)
-               PARITY = 3'b010,         //sending parity bit if enabled
-               STOP   = 3'b110,         //stop-bit = 1
-               DONE   = 3'b111;         //raise tx_done. go back to IDLE
+    //states encoding (One-Hot) to minimize glitches
+    localparam IDLE   = 6'b000001,                  //waits for tx_start = 1
+               START  = 6'b000010,                  //start-bit = 0
+               DATA   = 6'b000100,                  //transmit 8-bit data (LSB -> MSB)
+               PARITY = 6'b001000,                  //sending parity bit if enabled
+               STOP   = 6'b010000,                  //stop-bit = 1
+               DONE   = 6'b100000;                  //raise tx_done. go back to IDLE
 
-    uart_tx #(                          //rart_tx instantiation : dut
+    uart_tx #(                                      //dut_tx instantiation
         .BAUD(BAUD), 
         .clk_freq(clk_freq), 
+        .clk_period(clk_period),
         .oversampling_rate(oversampling_rate), 
         .data_wd(data_wd), 
         .parity(parity)
@@ -46,7 +49,7 @@ module tb_tx();
         .tx_busy(tx_busy)
     );
 
-    uart_baudgen #(                     //uart_baudgen instantiation to maintain real tick signal
+    uart_baudgen #(                                 //baudgen instantiation to maintain real tick signal
         .BAUD(BAUD), 
         .clk_freq(clk_freq), 
         .oversampling_rate(oversampling_rate)
@@ -57,67 +60,102 @@ module tb_tx();
         .tick(tick)
     );
 
-    initial                             //clk generation
+    initial                                         //clk generation
         begin
             clk = 0;
-            forever # 10 clk = ~clk;    //forcing clk period = 20, to maintain 50 MHZ frequency
+            forever #(clk_period/2) clk = ~clk;     //forcing clk period = 20, to maintain 50 MHZ frequency
         end
 
     initial
-        begin
+        begin                                       //waveform file
             $dumpfile("wave.vcd");
             $dumpvars(0, tb_tx);
         end
 
+    // TASKs
+    // reset task
+    task reset();
+        begin
+            rst = 1;        //apply rst
+            tx_start = 0;   //disables transmittion operation
+            din = 0;        //default value
+            repeat(10)      //waits for 10 clk cycles to track rst signal
+                @(negedge clk);
+            rst = 0;        //release rst
+        end    
+    endtask
+
+    // send_data task
+    task send_data(
+        input [data_wd-1 : 0] data
+    );
+        begin
+            din = data;     
+            tx_start = 1;           //allow transmittion operation [IDLE => START] transition
+            repeat(oversampling_rate)
+                @(negedge tick);    //sending start-bit [START => DATA] transition 
+            repeat(data_wd)
+                repeat(oversampling_rate)
+                    @(negedge tick);//sending all data bits [DATA => PARITY] transition
+        end
+    endtask
+
+    // waits for parity_bit task
+    task parity_bit();
+        begin
+            repeat(oversampling_rate * (3/4))
+                @(negedge tick);
+            if(tx)
+                $display("xxxx Error : parity-error | din = %b, odd-parity bit = %d")
+        end
+    endtask
+
     initial
         begin
-            $display("\n==================== RESET ====================\n");
-            rst = 1; tx_start = 0; din = 8'b0; 
-            repeat(10) @(negedge clk)               //activate rst
-            rst = 0;                 
-            repeat(10) @(negedge clk)               //release rst
-
-                        // scenario 1 : send a single byte (0xA5)
-            $display("\n=>>> scenario 1 : send a single byte (0xA5).=============\n");
-            din = 8'hA5;  tx_start = 1; @(negedge clk) ;       //IDLE => START transition    
-            tx_start = 0;
-            repeat(oversampling_rate) @(negedge tick);         //START => DATA transition
-            repeat(oversampling_rate * data_wd)                //DATA => PARITY transition
-                @(negedge tick);                
-            //repeat(oversampling_rate) @(negedge tick);       //PARITY => STOP transition : uncomment this line if parity_en is high
-            repeat(oversampling_rate) @(negedge tick);         //STOP => DONE transition
-            if(tx_done && !tx_busy && tx)
-                $display("Pass : scenario 1 (send a single byte (0xA5))");
+                                // 1st scenario Functional Correctness : (Reset Behavior)
+            $display("\n==================== 1st scenario Functional Correctness : (Reset Behavior) ====================");
+            reset();            
+            if(!tx || tx_done || tx_busy || dut_tx.tick_count != 0 || dut_tx.bit_index != 0 || dut_tx.c_state != IDLE)
+                $display("[FAIL] 1st scenario (Reset Behavior) : tx = %d, tx_done = %d, tx_busy = %d, tick_count = %d, bit_index = %d, c_state = %b", 
+                        tx, tx_done, tx_busy, dut_tx.tick_count, dut_tx.bit_index, dut_tx.c_state);
             else 
-                $display("Error : scenario 1 (send a single byte (0xA5)) | tx_done = %d, c_state = %b", tx_done, dut_tx.c_state);
+                $display("[PASS] 1st scenario (Reset Behavior)");
 
-                        //scenario 2 : send another byte (random)
-            $display("\n=>>> scenario 2 : send another byte (random).==============\n");
-            din = $random; tx_start = 1; @(negedge clk);         //IDLE => START transition  
-            tx_start = 0;  
-            repeat(oversampling_rate) @(negedge tick);           //START => DATA transition
-            repeat(oversampling_rate * data_wd) @(negedge tick); //DATA => PARITY transition
-            //repeat(oversampling_rate) @(negedge tick);         //PARITY => STOP transition : uncomment this line if parity_en is high
-            repeat(oversampling_rate) @(negedge tick);           //STOP => DONE transition
-            if(tx_done && !tx_busy && tx)
-                $display("Pass : scenario 2 (send another byte (random))");
-            else 
-                $display("Error : scenario 2 (send another byte (random)) | tx_done = %d, c_state = %b", tx_done, dut_tx.c_state);
+                                // 2nd scenario Functional Correctness : (Transmit Frame with odd-parity)
+            $display("\n==================== 2nd scenario Functional Correctness : (Transmit Frame with odd-parity) ====================");
+            reset();  
 
-                        //scenario 3 : rst during sending a frame
-            $display("\b=>>> scenario 3 : rst during sending a frame.===============\n");
-            din = $random; tx_start = 1; @(negedge clk);             //IDLE => START transition    
-            repeat(oversampling_rate) @(negedge tick);               //START => DATA transition
-            repeat(oversampling_rate * (data_wd/2)) @(negedge tick); //DATA => PARITY transition
-            rst = 1; @(negedge clk);                                 //applying rst after sending half of a single byte
-            if(tx && !tx_done && !tx_busy && (dut_tx.bit_index == 0) && (dut_tx.tick_count == 0) && (dut_tx.c_state == IDLE))
-                $display("Pass : scenario 3 (rst during sending a frame)");
-            else
-                $display("Error : scenario 3 (rst during sending a frame) | tx = %d, tx_done = %d, tx_busy = %d, bit_index = %d, tick_count = %d, c_state = %b", 
-                        tx, tx_done, tx_busy, dut_tx.bit_index, dut_tx.tick_count, dut_tx.c_state);
-            #100; $stop;
+                                // 3rd scenario Functional Correctness : (Transmit Multiple Back-to-Back Frames[10 frames])
+            $display("\n==================== 3rd scenario Functional Correctness : (Transmit Multiple Back-to-Back Frames[10 frames]) ====================");
+            reset();  
+
+                                // 4th scenario Corner Case : (tx_start is high during Transmitting a frame)
+            $display("\n==================== 4th scenario Corner Case : (tx_start is high during Transmitting a frame) ====================");
+            reset();  
+
+                                // 5th scenario Corner Case : (Reset during transmittion operation)
+            $display("\n==================== 5th scenario Corner Case : (Reset during transmittion operation) ====================");
+            reset();  
+
+                                // 6th scenario Corner Case : (Transmit a frame [0xFF])
+            $display("\n==================== 6th scenario Corner Case : (Transmit a frame [0xFF]) ====================");
+            reset();  
+
+                                // 7th scenario Corner Case : (Transmit a frame [0x00])
+            $display("\n==================== 7th scenario Corner Case : (Transmit a frame [0x00]) ====================");
+            reset(); 
+
+                                // 8th scenario Corner Case : ('din' changes during transmittion operation)
+            $display("\n==================== 3rd scenario (Transmit Multiple Back-to-Back Frames[10 frames]) ====================");
+            reset();  
+
+                                // STOP Simulatoin
+            $display("\n==================== STOP Simulatoin ====================");
+            reset();  
+            #100; 
+            $stop;
         end
 
-    initial
-        $monitor("@time (%t) : c_state = %b, tx_done = %d, tx_busy = %d, tx = %d", $time, dut_tx.c_state, tx_done, tx_busy, tx);
+    // initial
+    //     $monitor("@time (%t) : c_state = %b, tx_done = %d, tx_busy = %d, tx = %d", $time, dut_tx.c_state, tx_done, tx_busy, tx);
 endmodule
