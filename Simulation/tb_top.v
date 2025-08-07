489ab92f86daf895d940f43ffe3acbf873aa053b
Kareem Ashraf <ashrafkareem825@gmail.com>
2025-08-07 09:38:59 +0300

    add TX task

--- Simulation/tb_top.v
+++ Simulation/tb_top.v
@@ -44,14 +44,11 @@ module tb_top();
         wire parity_error_flag;                         //indicates invalid parity-bit's been received
         wire [data_wd-1 : 0] dout;                      //system parallel output data     
 
-    // Need Internal Signals 
-        reg start_bit_successful = 0;                   //asserts that the start-bit's been transmitted successfully 
-        reg stop_bit_successful = 0;                    //asserts that the stop-bit's been transmitted successfully 
-        reg data_bits_successful = 0;                   //asserts that all data bits have been transmitted successfully
-        reg [data_wd-1 : 0] test_data = 8'hFA;                  //register stores the data to test it
+    // Needed Internal Signals 
+        reg [data_wd-1 : 0] test_data;                  //register stores the data to test it
         integer i;                                      
 
-    // States Encoding (One-Hot) to minimize glitches
+    // State Encoding (One-Hot) to minimize glitches
         localparam IDLE   = 6'b000001,                  //waits for tx_start = 1
                    START  = 6'b000010,                  //start-bit = 0
                    DATA   = 6'b000100,                  //transmit 8-bit data (LSB -> MSB)
@@ -119,14 +116,64 @@ module tb_top();
                     tx_start = 0;
                     rx_start = 0;
                     din = 0;        // default data
-                    repeat(3)       // waits for 3 clk cycles to track rst signal
-                        @(negedge clk);
+                    @(negedge clk); // waits for a clk cycle to track reset signal
                     rst = 0;        // release rst
                 end
             endtask
 
-        // 
+        // TX task
+            task TX(input [data_wd-1 : 0] data); begin
+                // assign data
+                    din = data;         // assign input data
+                    tx_wr_en = 1;       // enables write operation to fifo_tx
+                    tx_rd_en = 1;       // enables read operation from fifo_tx
+                    tx_start = 1;       // enables transmittion operation
+                    rx_wr_en = 1;
+                    rx_rd_en = 1;
+                    @(negedge clk);     // waits to maintain (IDLE => START) transition
+                    if(DUT.TX.c_state == START) 
+                        $display("[PASS] (IDLE => START) transition | TX.c_state = %b, expected = %b", DUT.TX.c_state, START);
+                    else    
+                        $display("[FAIL] (IDLE => START) transition | TX.c_state = %b, expected = %b", DUT.TX.c_state, START);
+                // start-bit
+                    @(negedge DUT.tick);    // waits for a tick pulse to send start-bit
+                    if(DUT.RX.rx == 1'b0)   // start-bit assertion
+                        $display("[PASS] start-bit | start-bit sent = %d, expected = %d", DUT.RX.rx, 1'b0);
+                    else    
+                        $display("[FAIL] start-bit | start-bit sent = %d, expected = %d", DUT.RX.rx, 1'b0);
+                    repeat(oversampling_rate-1) @(negedge DUT.tick);
+                // data bits
+                    for(i=0; i<data_wd; i=i+1)begin
+                        repeat(2) @(negedge DUT.tick);
+                        if(DUT.TX.c_state == DATA && DUT.RX.rx == data[i])
+                            $display("[PASS] data-bit[%d] sent = %d, expected = %d, TX.c_state = %b, expected = %b", i, DUT.RX.rx, data[i], DUT.TX.c_state, DATA);
+                        else
+                            $display("[FAIL] data-bit[%d] sent = %d, expected = %d, TX.c_state = %b, expected = %b", i, DUT.RX.rx, data[i], DUT.TX.c_state, DATA);
+                        repeat(oversampling_rate-2) @(negedge DUT.tick);
+                    end
+                // parity-bit
+                    repeat(2) @(negedge DUT.tick);  // waits to send parity-bit
+                    if(DUT.TX.c_state == PARITY && DUT.RX.rx == DUT.TX.parity_res)
+                        $display("[PASS] parity-bit | parity-bit sent = %d, expected = %d, TX.c_state = %b, expected = %b", DUT.RX.rx, DUT.TX.parity_res, DUT.TX.c_state, PARITY);
+                    else    
+                        $display("[FAIL] parity-bit | parity-bit sent = %d, expected = %d, TX.c_state = %b, expected = %b", DUT.RX.rx, DUT.TX.parity_res, DUT.TX.c_state, PARITY);
+                    repeat(oversampling_rate-2) @(negedge DUT.tick);
+                // stop-bit
+                    @(negedge DUT.tick);    // waits for a tick pulse to send start-bit
+                    if(DUT.TX.c_state == STOP && DUT.RX.rx == 1'b1)      // stop-bit assertion
+                        $display("[PASS] stop-bit | stop-bit sent = %d, expected = %d, TX.c_state = %b, expected = %b", DUT.RX.rx, 1'b1, DUT.TX.c_state, STOP);
+                    else    
+                        $display("[FAIL] stop-bit | stop-bit sent = %d, expected = %d, TX.c_state = %b, expected = %b", DUT.RX.rx, 1'b1, DUT.TX.c_state, STOP);
+                    repeat(oversampling_rate-1) @(negedge DUT.tick);
+            end
+            endtask
 
+        // RX task
+            task RX(); begin
+                // assign data
+
+            end
+            endtask
     // Real World Scenarois & Corner Case tests
         initial 
             begin
@@ -135,18 +182,19 @@ module tb_top();
                 reset();    
                 if(tx_full || !tx_empty || tx_almost_full || !tx_almost_empty || rx_full || !rx_empty || rx_almost_full || !rx_almost_empty || 
                     tx_done || tx_busy || rx_done || rx_busy || framing_error_flag || parity_error_flag || DUT.TX.c_state != IDLE || DUT.RX.c_state != IDLE)
-                    $display("[FAIL] | tx_full = %d, tx_empty = tx_almost_full = %d, tx_almost_empty = %d, tx_done = %d, tx_busy = %d, \n rx_full = %d, rx_empty = rx_almost_full = %d, rx_almost_empty = %d, rx_done = %d, rx_busy = %d, \n framing_error_flag = %d, parity_error_flag = %d, TX.c_state = %b, RX.c_state = %b", 
+                    $display("[FAIL] | tx_full = %d, tx_empty = %d, tx_almost_full = %d, tx_almost_empty = %d, tx_done = %d, tx_busy = %d, \n rx_full = %d, rx_empty = %d, rx_almost_full = %d, rx_almost_empty = %d, rx_done = %d, rx_busy = %d, \n framing_error_flag = %d, parity_error_flag = %d, TX.c_state = %b, RX.c_state = %b", 
                                 tx_full, tx_empty, tx_almost_full, tx_almost_empty, tx_done, tx_busy, rx_full, rx_empty, rx_almost_full, rx_almost_empty, rx_done, rx_busy, 
                                 framing_error_flag, parity_error_flag, DUT.TX.c_state, DUT.RX.c_state);
                 else
-                    $display("[PASS] | tx_full = %d, tx_empty = tx_almost_full = %d, tx_almost_empty = %d, tx_done = %d, tx_busy = %d, \n rx_full = %d, rx_empty = rx_almost_full = %d, rx_almost_empty = %d, rx_done = %d, rx_busy = %d, \n framing_error_flag = %d, parity_error_flag = %d, TX.c_state = %b, RX.c_state = %b", 
+                    $display("[PASS] | tx_full = %d, tx_empty = %d, tx_almost_full = %d, tx_almost_empty = %d, tx_done = %d, tx_busy = %d, \n rx_full = %d, rx_empty = %d, rx_almost_full = %d, rx_almost_empty = %d, rx_done = %d, rx_busy = %d, \n framing_error_flag = %d, parity_error_flag = %d, TX.c_state = %b, RX.c_state = %b", 
                                 tx_full, tx_empty, tx_almost_full, tx_almost_empty, tx_done, tx_busy, rx_full, rx_empty, rx_almost_full, rx_almost_empty, rx_done, rx_busy, 
                                 framing_error_flag, parity_error_flag, DUT.TX.c_state, DUT.RX.c_state);
 
             // 2nd scenario Functional Correctness (Basic TX Transmittion)
                 $display("\n ==================== 2nd scenario Functional Correctness (Basic TX Transmittion) ====================");
                 reset(); 
-
+                test_data = $random;
+                TX(test_data);      // send random data
             // 3rd scenario Functional Correctness (Basic RX Reception)
                 $display("\n ==================== 3rd scenario Functional Correctness (Basic RX Reception) ====================");
                 reset(); 
@@ -193,6 +241,6 @@ module tb_top();
                 $stop;
             end
     // monitor
-        // initial
-        //     $monitor("fifo [0] = %h, tx_rx = %d, test_data = %h, TX.c_state = %b", DUT.fifo_tx.fifo[0], DUT.tx_rx, test_data, DUT.TX.c_state);
+        initial
+            $monitor("tx_done = %d, rx_done = %d, tx_full = %d, tx_fifo_count = %d", tx_done, rx_done, tx_full, DUT.fifo_tx.fifo_count);
 endmodule
